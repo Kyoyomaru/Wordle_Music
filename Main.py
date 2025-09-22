f4aba44c17ccec9c960cf1ce6f3a66625e7200ec
jhoan alexis <davidtuberquiajhoanalexis@gmail.com>
2025-09-22 17:54:54 -0500

    Clase Word y jugador

--- Main.py
+++ Main.py
@@ -35,16 +35,16 @@ class WordleMusic:
     def registrar_jugador(self, nombre: str) -> None:
         self.jugador: Jugador = Jugador(nombre)
 
-    def seleccionar_cancion(self) -> None:                  # Este metodo elige aleatoriamente una canción de la lista.
+    def seleccionar_cancion(self) -> None:
         self.cancion_actual = random.choice(self.canciones)
         self.intentos_restantes = self.max_intentos
 
-    def inicializar_tablero(self) -> int:                   # Devuelve el número de letras de la canción actual.
+    def inicializar_tablero(self) -> int:
         if not self.cancion_actual:
             raise ValueError("No hay canción seleccionada.")
         return len(self.cancion_actual.titulo)
 
-    def validar_intento(self, palabra: str) -> list[Letra]:    # Valida un intento contra el título actual.
+    def validar_intento(self, palabra: str) -> list[Letra]:
         if not self.cancion_actual:
             raise ValueError("No hay canción seleccionada.")
 
@@ -52,7 +52,7 @@ class WordleMusic:
         self.intentos_restantes -= 1
         return self.cancion_actual.comparar_letras(intento)
 
-    def finalizar_juego(self) -> str:                         # Retorna el título correcto de la canción.
+    def finalizar_juego(self) -> str:
         if not self.cancion_actual:
             raise ValueError("No hay canción seleccionada.")
         return self.cancion_actual.mostrar_titulo_correcto()
